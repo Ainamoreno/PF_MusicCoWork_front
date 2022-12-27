e5e0c05239300abf0269e1abe2a49ee16f85cef1
Aina <ainamoreno@outlook.com>
2022-12-27 19:04:11 +0100

    Creo about

---
+++ src/containers/About/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./About.scss";
+
+function About() {
+  return (
+    <Container fluid className="aboutDesign">
+      <Col>
+        <Row></Row>
+      </Col>
+      <div>About</div>
+    </Container>
+  );
+}
+
+export default About;

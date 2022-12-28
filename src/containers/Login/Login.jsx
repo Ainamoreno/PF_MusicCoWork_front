5235e9bb29b075aec469a6074a6fc682b0511076
Aina <ainamoreno@outlook.com>
2022-12-28 17:47:18 +0100

    Maquetación de la pantalla register y login

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import "./Login.scss";
 
+import { GiBeveledStar } from "react-icons/gi";
+import { BsFillPersonFill } from "react-icons/bs";
+import { BsPerson } from "react-icons/bs";
 function Login() {
   return (
-    <div>Login</div>
+    <Container fluid className="containerLogin">
+    <Row className="pt-5">
+      <Col className=" pt-3 d-flex justify-content-center">
+        <div className=" d-flex justify-content-center align-items-center flex-column">
+          <h1 className=" d-flex justify-content-center align-items-center titleConatinerLogin tracking-in-contract-bck">
+            Iniciar sesión
+          </h1>
+          <h6 className="mt-3">
+            Y podrás reservar nuestras salas y asistir a los mejores eventos
+            musicales de Valencia
+          </h6>
+        </div>
+      </Col>
+    </Row>
+    <Row className=" pt-3 d-flex justify-content-center">
+      <Col md={6} className=" pt-3 d-flex justify-content-center flex-column">
+        <InputGroup className="mb-3 ">
+          <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+          <Form.Control
+            type="email"
+            placeholder="Correo electrónico"
+            aria-label="e-mail"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+        <InputGroup className="mb-3 ">
+          <InputGroup.Text id="basic-addon1"><GiBeveledStar/></InputGroup.Text>
+          <Form.Control
+            type="password"
+            placeholder="Contraseña"
+            aria-label="password"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+      </Col>
+    </Row>
+    <Row>
+      <Col className=" mt-3 d-flex justify-content-center">
+      <Button variant="secondary">Iniciar sesión</Button>
+      </Col>
+    </Row>
+  </Container>
   )
 }
 
--- src/containers/Register/Register.jsx
+++ src/containers/Register/Register.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import "./Register.scss";
 
+import { GiBeveledStar } from "react-icons/gi";
+import { BsFillPersonFill } from "react-icons/bs";
+import { BsPerson } from "react-icons/bs";
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <Container fluid className="containerRegister">
+      <Row className="pt-5">
+        <Col className=" pt-3 d-flex justify-content-center">
+          <div className=" d-flex justify-content-center align-items-center flex-column">
+            <h1 className=" d-flex justify-content-center align-items-center titleConatinerRegister tracking-in-contract-bck">
+              Regístrate
+            </h1>
+            <h6 className="mt-3">
+              Y podrás reservar nuestras salas y asistir a los mejores eventos
+              musicales de Valencia
+            </h6>
+          </div>
+        </Col>
+      </Row>
+      <Row className=" pt-3 d-flex justify-content-center">
+        <Col md={6} className=" pt-3 d-flex justify-content-center flex-column">
+          <InputGroup className="pb-3 ">
+            <InputGroup.Text id="basic-addon1"><BsFillPersonFill/></InputGroup.Text>
+            <Form.Control
+              type="text"
+              placeholder="Nombre"
+              aria-label="name"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+          <InputGroup className="mb-3 ">
+            <InputGroup.Text id="basic-addon1"><BsPerson/></InputGroup.Text>
+            <Form.Control
+              type="text"
+              placeholder="Apellido"
+              aria-label="surname"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+          <InputGroup className="mb-3 ">
+            <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+            <Form.Control
+              type="email"
+              placeholder="Correo electrónico"
+              aria-label="e-mail"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+          <InputGroup className="mb-3 ">
+            <InputGroup.Text id="basic-addon1"><GiBeveledStar/></InputGroup.Text>
+            <Form.Control
+              type="password"
+              placeholder="Contraseña"
+              aria-label="password"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col className=" mt-3 d-flex justify-content-center">
+        <Button variant="secondary">Regístrate</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default Register
+export default Register;
